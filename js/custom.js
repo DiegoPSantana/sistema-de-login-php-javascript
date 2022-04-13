731b1160037ba2439d700b24ab43e9edf63572a7
DiegoPSantana <diego.fourstage@gmail.com>
2022-04-13 16:14:45 -0300

    Validando do lado do servidor(php)

--- js/custom.js
+++ js/custom.js
@@ -10,21 +10,25 @@ loginForm.addEventListener("submit", async (e) => {
     // Para que a página janela não seja carregada
     e.preventDefault();
 
-    // Validando se o campos de email e senha estão preenchidos
-    if (document.getElementById("email").value === "") {
-        // const será atribuida uma mensagem
-        msgAlertErroLogin.innerHTML = "<div class='alert alert-danger' role='alert'>Necessario preencher o campo usuário!</div>";
-    } else if(document.getElementById("senha").value === ""){
-        msgAlertErroLogin.innerHTML ="<div class='alert alert-danger' role='alert'>Necessario preencher o campo senha!</div>";
-    } else{
+    // // Validando se o campos de email e senha estão preenchidos
+    // if (document.getElementById("email").value === "") {
+    //     // const será atribuida uma mensagem
+    //     msgAlertErroLogin.innerHTML = "<div class='alert alert-danger' role='alert'>Necessario preencher o campo usuário!</div>";
+    // } else if (document.getElementById("senha").value === "") {
+    //     msgAlertErroLogin.innerHTML = "<div class='alert alert-danger' role='alert'>Necessario preencher o campo senha!</div>";
+    // } else {
         // Mas se usuário preencher todos os campos
         const dadosForm = new FormData(loginForm);
 
         // await => Aguardar, enquanto o processamento não finalizar esta
         // linha, não pode ir para próxima linha 
-        const dados = await fetch("validar.php",{
+        const dados = await fetch("validar.php", {
             method: "POST",
             body: dadosForm
         });
-    }
+
+        const resposta = await dados.json();
+
+        console.log(resposta)
+    //}
 })
---
+++ validar.php
@@ -0,0 +1,21 @@
+<?php
+
+// Atribuindo os dados e passando para todas String em uma variável
+$dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+// Validação do lado do servidor
+//Email
+if (empty($dados['email'])) {
+    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>
+    Necessario preencher o campo usuário!</div>"];
+    //Senha
+} elseif (empty($dados['senha'])) {
+    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>
+    Necessario preencher o campo senha!</div>"];
+} else {
+    // Validando email do lado do servidor
+    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-sucess' role='alert'>
+    Validar!</div>"];
+}
+
+echo json_encode($retorna);
